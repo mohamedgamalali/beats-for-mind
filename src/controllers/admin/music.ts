13c560bfacd61f65ec895466433f0c5eaf8248bf
mohamedgamalali <mg7404809@gmail.com>
2021-07-30 19:39:30 +0200

    dynamic update rate

--- src/controllers/admin/music.ts
+++ src/controllers/admin/music.ts
@@ -4,7 +4,7 @@ import { validationResult } from 'express-validator'
 import Catigory, { catigory } from '../../models/catigory';
 import Beet, { beet } from '../../models/beet';
 import { Types } from 'mongoose';
-import beetsData, {filesContainer} from '../../services/beetData';
+import beetsData, { filesContainer } from '../../services/beetData';
 
 export async function addCatigory(req: Request, res: Response, next: NextFunction) {
 
@@ -38,46 +38,51 @@ export async function adddBeet(req: Request, res: Response, next: NextFunction) 
         const images: any = req.files;
         let imagePath: string;
         let coverImagePath: string;
-        
-
-        // const errors = validationResult(req);
-        // if (!errors.isEmpty()) {
-        //     return response.ValidationFaild(res, 'validation faild', errors.array())
-        // }
-
-        // if (images.length == 0) {
-        //     return response.ValidationFaild(res, 'you should at least insert one image')
-        // }
-
-        // imagePath = images[0].path;
-
-        // if (!images[1]) {
-        //     coverImagePath = imagePath;
-        // } else {
-        //     coverImagePath = images[1].path;
-        // }
-
-        // const cat = await Catigory.findById(catigory);
-
-        // if (!cat) {
-        //     return response.NotFound(res, 'catigory not found');
-        // }
-
-        // const newBeet = new Beet({
-        //     name: name,
-        //     image: imagePath,
-        //     coverImage: coverImagePath,
-        //     beet: ,
-        //     duration: string,
-        //     hide: boolean,
-        //     catigory: Types.ObjectId
 
 
-        // })
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return response.ValidationFaild(res, 'validation faild', errors.array())
+        }
 
-        const data:filesContainer = await beetsData.files(images)
+        if (images.length == 0) {
+            return response.ValidationFaild(res, 'you should at least insert one file')
+        }
 
-        return response.created(res, 'catigory created', data);
+
+        const data: filesContainer = await beetsData.files(images)
+
+        if (data.audio == '') {
+            return response.ValidationFaild(res, 'you should insert audio')
+        }
+
+        if (data.image == '') {
+            return response.ValidationFaild(res, 'you should at least insert beet image')
+        }
+        if(data.coverImage == ''){
+            coverImagePath = data.image ;
+        }else{
+            coverImagePath = data.coverImage ;
+        }
+
+        const cat = await Catigory.findById(catigory)
+        if(!cat){
+            return response.NotFound(res, 'catigory not found')
+        }
+        const newBeet = new Beet({
+            name: name,
+            image: data.image,
+            coverImage: coverImagePath,
+            beet: data.audio,
+            catigory: Types.ObjectId
+        })
+
+        await newBeet.save() ;
+
+
+        return response.created(res, 'catigory created', {
+            beet:newBeet
+        });
 
     } catch (err) {
 
