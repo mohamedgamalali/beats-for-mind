8e68a4fdbbd7a12eca21eb4faaebdbebe91d4f37
mohamedgamalali <mg7404809@gmail.com>
2021-08-02 19:36:40 +0200

    make optional auth for get beats

--- src/routes/user/music.ts
+++ src/routes/user/music.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import * as musicController from '../../controllers/user/music';
 import { body } from 'express-validator'
 
-import isAuth from '../../helpers/isAuthUserQ'
+import isAuth from '../../helpers/isAuthUser'
 import isAuthOptional from '../../helpers/isAuthOptional'
 
 const router = Router();
